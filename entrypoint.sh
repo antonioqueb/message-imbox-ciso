69c2dc1c733257936543cc167bf29d0f146cfa44
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-24 21:06:34 -0600

    Update

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Check if the database file exists
+if [ ! -f "/app/instance/messages.db" ]; then
+  echo "Database not found. Initializing database..."
+  flask db init || echo "Migrations already initialized"
+  flask db migrate -m "Initial migration"
+  flask db upgrade
+else
+  echo "Database found. Skipping initialization."
+fi
+
+# Run the application
+flask run --host=0.0.0.0
